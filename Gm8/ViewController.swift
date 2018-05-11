1f22a97e8512a0d898919a631002a8d0198198a9
Sasha <vdovicenco.alexandr@gmail.com>
2018-05-11 23:31:22 +0300

    Initial Commit

---
+++ Gm8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gm8
+//
+//  Created by Hackintosh on 5/11/18.
+//  Copyright © 2018 Hackintosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
